09bac991ac51b793814e5be17aee54b9ae0f9a71
iOSDevLime <onthelots@naver.com>
2023-02-21 23:51:58 +0900

    Add Files

---
+++ Ditor/Ditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ditor
+//
+//  Created by Jae hyuk Yim on 2023/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
