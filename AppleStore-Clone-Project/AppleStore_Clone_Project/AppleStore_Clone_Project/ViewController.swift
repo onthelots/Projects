ea15da7bfeb0007a9902a7601b4422b656b66f61
iOSDevLime <onthelots@naver.com>
2023-04-10 21:12:18 +0900

    AppStore Clone project 폴더 생성

---
+++ AppleStore-Clone-Project/AppleStore_Clone_Project/AppleStore_Clone_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleStore_Clone_Project
+//
+//  Created by Jae hyuk Yim on 2023/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
