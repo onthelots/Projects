c6eb155c42c587a2d8af1fa60955201ed32ad635
iOSDevLime <onthelots@naver.com>
2023-04-23 17:36:18 +0900

    iTunes Api를 활용한 클론 프로젝트 시작

---
+++ AppleStore Clone/AppleStore_Clone/AppleStore_Clone/Main/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  AppleStore_Clone
+//
+//  Created by Jae hyuk Yim on 2023/04/23.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AppleStore Clone/AppleStore_Clone/AppleStore_Clone/Search/SearchCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchCell.swift
+//  AppleStore_Clone
+//
+//  Created by Jae hyuk Yim on 2023/04/23.
+//
+
+import UIKit
+
+class SearchCell: UICollectionViewCell {
+    
+}
